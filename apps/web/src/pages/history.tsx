cf7c58e53663c5bcb868946734c87469c2ebee10
Vitor Windberg <vitorwlima13@gmail.com>
2024-01-29 23:42:35 -0300

    feat: add missing pages base structure

--- apps/web/src/pages/history.tsx
+++ apps/web/src/pages/history.tsx
@@ -1,7 +1,11 @@
 export const History: React.FC = () => {
 	return (
 		<div>
-			<h1>History</h1>
+			<header className="flex items-center justify-between mb-8">
+				<h2 className="text-xl font-semibold">History</h2>
+			</header>
+
+			<p className="text-muted-foreground">In progress. Check again soon.</p>
 		</div>
 	)
 }
--- apps/web/src/pages/rewards.tsx
+++ apps/web/src/pages/rewards.tsx
@@ -1,3 +1,11 @@
 export const Rewards: React.FC = () => {
-	return <h1>Rewards</h1>
+	return (
+		<div>
+			<header className="flex items-center justify-between mb-8">
+				<h2 className="text-xl font-semibold">Rewards</h2>
+			</header>
+
+			<p className="text-muted-foreground">In progress. Check again soon.</p>
+		</div>
+	)
 }
