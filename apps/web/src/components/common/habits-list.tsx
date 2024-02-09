234c2c2c016f336a4c4d2c06ea190a3b70cf8904
Vitor Windberg <vitorwlima13@gmail.com>
2024-02-09 16:17:05 -0300

    refactor: use habits list hook

--- apps/web/src/components/common/habits-list.tsx
+++ apps/web/src/components/common/habits-list.tsx
@@ -6,27 +6,11 @@ import {
 	TableHead,
 	TableHeader,
 } from '@/components/ui/table'
-import { trpc } from '@/lib/trpc'
-import { useUser } from '@clerk/clerk-react'
-import {
-	DndContext,
-	DragEndEvent,
-	KeyboardSensor,
-	PointerSensor,
-	closestCenter,
-	useSensor,
-	useSensors,
-} from '@dnd-kit/core'
-import {
-	SortableContext,
-	arrayMove,
-	sortableKeyboardCoordinates,
-	verticalListSortingStrategy,
-} from '@dnd-kit/sortable'
-import { useQueryClient } from '@tanstack/react-query'
-import { getQueryKey } from '@trpc/react-query'
+import { useHabitsList } from '@/lib/component-hooks/use-habits-list'
+import { DndContext, closestCenter } from '@dnd-kit/core'
+import { SortableContext, verticalListSortingStrategy } from '@dnd-kit/sortable'
 
-type Props = {
+export type Props = {
 	habits: {
 		userId: string
 		id: string
@@ -36,21 +20,7 @@ type Props = {
 }
 
 export const HabitsList: React.FC<Props> = ({ habits }) => {
-	const { user } = useUser()
-	const queryClient = useQueryClient()
-	const { mutate } = trpc.habit.changeHabitsOrder.useMutation()
-	const habitsKey = getQueryKey(trpc.habit.getHabits)
-
-	const sensors = useSensors(
-		useSensor(PointerSensor, {
-			activationConstraint: {
-				distance: 10,
-			},
-		}),
-		useSensor(KeyboardSensor, {
-			coordinateGetter: sortableKeyboardCoordinates,
-		}),
-	)
+	const { sensors, handleDragEnd } = useHabitsList({ habits })
 
 	if (!habits.length) {
 		return (
@@ -58,40 +28,6 @@ export const HabitsList: React.FC<Props> = ({ habits }) => {
 				You have not created any habits yet. Click the button above to start.
 			</p>
 		)
-	}
-
-	const handleDragEnd = (event: DragEndEvent) => {
-		const { active, over } = event
-
-		if (active.id !== over?.id) {
-			const oldIndex = habits.findIndex((h) => h.id === active.id)
-			const newIndex = habits.findIndex((h) => h.id === over?.id)
-
-			const habitIdsInOrder = arrayMove(habits, oldIndex, newIndex).map(
-				(h) => h.id,
-			)
-
-			mutate({
-				habitIdsInOrder,
-				userId: user!.id,
-			})
-
-			queryClient.setQueriesData(habitsKey, (old) => {
-				const oldHabits = (old as { habits: [] })?.habits as {
-					id: string
-					name: string
-					days: string
-					userId: string
-				}[]
-
-				const habits = oldHabits.map((h) => ({
-					...h,
-					order: habitIdsInOrder.indexOf(h.id),
-				}))
-
-				return { habits }
-			})
-		}
 	}
 
 	return (
---
+++ apps/web/src/lib/component-hooks/use-habits-list.ts
@@ -0,0 +1,73 @@
+import { Props } from '@/components/common/habits-list'
+import { trpc } from '@/lib/trpc'
+import { useUser } from '@clerk/clerk-react'
+import {
+	DragEndEvent,
+	KeyboardSensor,
+	PointerSensor,
+	useSensor,
+	useSensors,
+} from '@dnd-kit/core'
+import { arrayMove, sortableKeyboardCoordinates } from '@dnd-kit/sortable'
+import { useQueryClient } from '@tanstack/react-query'
+import { getQueryKey } from '@trpc/react-query'
+import { toast } from 'sonner'
+
+export const useHabitsList = ({ habits }: Props) => {
+	const { user } = useUser()
+	const { mutate } = trpc.habit.changeHabitsOrder.useMutation()
+	const queryClient = useQueryClient()
+	const habitsKey = getQueryKey(trpc.habit.getHabits)
+
+	const sensors = useSensors(
+		useSensor(PointerSensor, {
+			activationConstraint: {
+				distance: 10,
+			},
+		}),
+		useSensor(KeyboardSensor, {
+			coordinateGetter: sortableKeyboardCoordinates,
+		}),
+	)
+
+	const handleDragEnd = (event: DragEndEvent) => {
+		const { active, over } = event
+
+		if (active.id !== over?.id) {
+			const oldIndex = habits.findIndex((h) => h.id === active.id)
+			const newIndex = habits.findIndex((h) => h.id === over?.id)
+
+			const habitIdsInOrder = arrayMove(habits, oldIndex, newIndex).map(
+				(h) => h.id,
+			)
+
+			mutate({
+				habitIdsInOrder,
+				userId: user!.id,
+			})
+
+			queryClient.setQueriesData(habitsKey, (old) => {
+				const oldHabits = (old as { habits: [] })?.habits as {
+					id: string
+					name: string
+					days: string
+					userId: string
+				}[]
+
+				const habits = oldHabits.map((h) => ({
+					...h,
+					order: habitIdsInOrder.indexOf(h.id),
+				}))
+
+				return { habits }
+			})
+
+			toast.success('Habit order updated successfully.')
+		}
+	}
+
+	return {
+		sensors,
+		handleDragEnd,
+	}
+}
