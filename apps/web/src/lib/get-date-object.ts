868aa9f0c6a4280cad3b2d5dc6ce6e69699203d4
Vitor Windberg <vitorwlima13@gmail.com>
2024-02-14 23:38:49 -0300

    fix: attempt to fix crazy timezone issues

--- apps/web/src/lib/get-date-object.ts
+++ apps/web/src/lib/get-date-object.ts
@@ -1,8 +1,10 @@
 import { getTime } from 'date-fns'
 
 export const getDateObject = (date?: Date | string) => {
-	const userTimezoneOffset = new Date().getTimezoneOffset() * 60 * 1000
-	const dateObject = date ? new Date(date) : new Date()
+	const userTimezoneOffset = 0
+	const dateObject = date
+		? new Date(typeof date === 'string' ? date.replace('-', '/') : date)
+		: new Date()
 
 	return new Date(getTime(dateObject) + userTimezoneOffset)
 }
